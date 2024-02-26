91687a35f5f294399e92b6d38350285a73c78503
HanielUlises <hanielulises2003@gmail.com>
2024-02-26 14:19:56 -0600

    Messed up a bit

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 CXX = g++
 CXXFLAGS = -std=c++20 -Wall -Wextra -pedantic
 LDFLAGS =
+
 OBJ = main.o image_handler.o Utils.o
+
 DEPS = image_handler.h Utils.h
+
 TARGET = prediction
 
 all: $(TARGET)
 
-$(TARGET): $(OBJ)s
+$(TARGET): $(OBJ)
 	$(CXX) -o $@ $^ $(LDFLAGS)
 
 %.o: %.cpp $(DEPS)
--- main.cpp
+++ main.cpp
@@ -4,13 +4,16 @@
 #include <iostream>
 
 int main() {
-    std::string image_path1 = "./imagen.bmp";
-    std::string image_path2 = "./imagen2.bmp";
+    std::string image_path1 = "image1.bmp";
+    std::string image_path2 = "image2.bmp";
 
     Image test_image1(image_path1);
     Image test_image2(image_path2);
 
-    double threshold = 150;
+    test_image1.fulfill_matrix();
+    test_image2.fulfill_matrix();
+
+    double threshold = 0.90;
     get_prediction(test_image1, test_image2, threshold);
 
     return 0;
