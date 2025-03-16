d028d841450d14a260ed6e7958045fada6f78168
HanielUlises <hanielulises2003@gmail.com>
2025-03-15 20:45:30 -0600

    Tracking and Annotation

---
+++ Object tracking/Face Annotation/build/CMakeFiles/RectangleDrawing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RectangleDrawing.dir/main.cpp.o"
+  "CMakeFiles/RectangleDrawing.dir/main.cpp.o.d"
+  "RectangleDrawing"
+  "RectangleDrawing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RectangleDrawing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
