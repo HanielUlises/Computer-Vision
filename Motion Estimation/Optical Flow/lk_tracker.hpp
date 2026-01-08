1249382110952181a26ec9c5d9e7368d2f29965a
HanielUlises <hanielulises2003@gmail.com>
2026-01-07 21:34:37 -0600

    Lucas-Kanade tracker

---
+++ Motion Estimation/Optical Flow/lk_tracker.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+class LKTracker {
+    public:
+        LKTracker(
+            int win_size = 21,
+            int max_level = 0,
+            int max_iters = 30,
+            double epsilon = 0.01
+        );
+
+        void track(
+            const cv::Mat& prev_img,
+            const cv::Mat& curr_img,
+            const std::vector<cv::Point2f>& prev_pts,
+            std::vector<cv::Point2f>& curr_pts,
+            std::vector<uchar>& status
+        );
+
+    private:
+        cv::Size win_size_;
+        cv::TermCriteria termcrit_;
+        int max_level_;
+};
